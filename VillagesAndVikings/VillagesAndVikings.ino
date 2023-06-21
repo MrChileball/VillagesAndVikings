8056e47c68d9dfa4a51f6478d828fabe527c843f
MrChileball <89463623+MrChileball@users.noreply.github.com>
2023-06-20 21:26:21 -0400

    Update VillagesAndVikings.ino
    Actualización de balanceo, ajustes a la probabilidad de saqueo y guardado de partida

--- VillagesAndVikings/VillagesAndVikings.ino
+++ VillagesAndVikings/VillagesAndVikings.ino
@@ -1,5 +1,6 @@
 #include <EEPROM.h>
 
+
 // Variables de juego
 
 uint8_t poblacion = 20;
@@ -12,16 +13,20 @@ int dinero= 5; // Dinero inicial
 int ingreso;
 const byte ingresoBase = 5;
 int turno = 0;
-byte turnosRecaudacion = 10;
+const byte turnosRecaudacion = 10;
 
 // Direcciones de memoria en la EEPROM para cada variable
 #define ADDR_POBLACION 60
 #define ADDR_LIMITE_POBLACION 61
-#define ADDR_RECURSOS 62
-#define ADDR_LIMITE_RECURSOS 63
-#define ADDR_DINERO 64
-#define ADDR_TURNO 65
-#define ADDR_PROBABILIDAD_INVASION 66
+
+#define ADDR_RECURSOS 70
+#define ADDR_LIMITE_RECURSOS 71
+
+#define ADDR_DINERO 100
+#define ADDR_INGRESO 101
+
+#define ADDR_TURNO 45
+#define ADDR_PROBABILIDAD_INVASION 50
 
 
 
@@ -38,7 +43,6 @@ void setup() {
   // Muestra el mensaje de bienvenida y comandos
   Serial.println(F("¡Bienvenido a VillagesAndVikings!")); // la función F() almacena cadenas de texto en la flash, se ahorra MUCHA sram!
   mostrarComandos(); //Muestra el mensaje de comandos disponibles
-  
 
   /* NO SE DEBEN CARGAR LOS DATOS DE GUARDADO EN EL VOIDSETUP NI VOIDLOOP
   Explicación: La memoria EEPROM tiene una capacidad limite de entre 100 mil a 1 millón de escritura, 
@@ -54,63 +58,6 @@ void loop() {
     ingresosContador();
   }
 }
-
-
-
-void manejarComando(String comando) {
-  // Convertir el comando a minúsculas
-  comando.toLowerCase();
-
-  if (comando == "ayuda" || comando == "a") {
-    mostrarComandos();
-  } else if (comando == "recolectar" || comando == "r") {
-    turno++;
-    recolectarRecursos();
-    probabilidadInvasion += 14;
-  } else if (comando == "usar" || comando == "u") {
-    turno++;
-    usarRecursos();
-    probabilidadInvasion += 14;
-  } else if (comando == "almacén" || comando == "almacen" || comando == "a") {
-    turno++;
-    construirAlmacen();
-    probabilidadInvasion += 14;
-  } else if (comando == "expandir" || comando == "e") {
-    turno++;
-    expandirAldea();
-    probabilidadInvasion += 14;
-  } else if ( comando == "guardar") {
-    guardarVariablesEnEEPROM();
-  } else if ( comando == "cargar") {
-    cargarVariablesDesdeEEPROM();
-  } else if (comando == "test1") {
-    eventoAtaqueVikingo();
-  } else if (comando == "info" || comando == "i") {
-    mostrarInfo();
-  } else {
-    Serial.print(prefixInfo);
-    Serial.println(F("Comando inválido. Introduce 'ayuda' para ver la lista de comandos."));
-  }
-  
-}
-
-void mostrarComandos() {
-  Serial.println(F("╔════════════════════════════════╗"));
-  Serial.println(F("          MENÚ DE AYUDA:           "));
-  Serial.println(F("╚════════════════════════════════╝"));
-  Serial.println();
-  Serial.println(F("Juego:"));
-  Serial.println(F("- recolectar (r): recolecta 5 recursos"));
-  Serial.println(F("- usar (u): usa 5 recursos y añade 2 de población"));
-  Serial.println(F("- almacén (a): construye un almacén para aumentar el límite de recursos posibles (+10)"));
-  Serial.println(F("- expandir (e): consume 15 recursos y aumenta el límite de población en 8"));
-  Serial.println(F("Ayuda:"));
-  Serial.println(F("- info (i): muestra un resumen de la información del juego"));
-  Serial.println(F("Partida:"));
-  Serial.println(F("- guardar: guarda la partida en la EEPROM"));
-  Serial.println(F("- cargar: carga las partida de la EEPROM"));
-}
-
 void recolectarRecursos() {
   if (recursos + 5 <= limiteRecursos) {
     recursos += 5;
@@ -160,6 +107,22 @@ void expandirAldea() {
   }
 }
 
+void mostrarComandos() {
+  Serial.println(F("╔════════════════════════════════╗"));
+  Serial.println(F("          MENÚ DE AYUDA:           "));
+  Serial.println(F("╚════════════════════════════════╝"));
+  Serial.println();
+  Serial.println(F("Juego:"));
+  Serial.println(F("- recolectar (r): recolecta 5 recursos"));
+  Serial.println(F("- usar (u): usa 5 recursos y añade 2 de población"));
+  Serial.println(F("- almacén (a): construye un almacén para aumentar el límite de recursos posibles (+10)"));
+  Serial.println(F("- expandir (e): consume 15 recursos y aumenta el límite de población en 8"));
+  Serial.println(F("Ayuda:"));
+  Serial.println(F("- info (i): muestra un resumen de la información del juego"));
+  Serial.println(F("Partida:"));
+  Serial.println(F("- guardar: guarda la partida en la EEPROM"));
+  Serial.println(F("- cargar: carga las partida de la EEPROM"));
+}
 void mostrarInfo() {
   // Imprimir la parte superior de la cuadrícula
   Serial.println(F("╔═════════════════════════════════╗"));
@@ -184,6 +147,43 @@ void mostrarInfo() {
 
   // Imprimir la parte inferior de la cuadrícula
   Serial.println(F("╚═════════════════════════════════╝"));
+}
+
+
+void manejarComando(String comando) {
+  // Convertir el comando a minúsculas
+  comando.toLowerCase();
+  if (comando == "ayuda" || comando == "a") {
+    mostrarComandos();
+  } else if (comando == "recolectar" || comando == "r") {
+    turno++;
+    recolectarRecursos();
+    probabilidadInvasion += 6;
+  } else if (comando == "usar" || comando == "u") {
+    turno++;
+    usarRecursos();
+    probabilidadInvasion += 6;
+  } else if (comando == "almacén" || comando == "almacen" || comando == "a") {
+    turno++;
+    construirAlmacen();
+    probabilidadInvasion += 8;
+  } else if (comando == "expandir" || comando == "e") {
+    turno++;
+    expandirAldea();
+    probabilidadInvasion += 10;
+  } else if ( comando == "guardar") {
+    guardarVariablesEnEEPROM();
+  } else if ( comando == "cargar") {
+    cargarVariablesDesdeEEPROM();
+  } else if (comando == "test1") {
+    eventoAtaqueVikingo();
+  } else if (comando == "info" || comando == "i") {
+    mostrarInfo();
+  } else {
+    Serial.print(prefixInfo);
+    Serial.println(F("Comando inválido. Introduce 'ayuda' para ver la lista de comandos."));
+  }
+  
 }
 
 void gestorDinero (){
@@ -239,6 +239,7 @@ void cargarVariablesDesdeEEPROM() {
   EEPROM.get(ADDR_RECURSOS, recursos);
   EEPROM.get(ADDR_LIMITE_RECURSOS, limiteRecursos);
   EEPROM.get(ADDR_DINERO, dinero);
+  EEPROM.get(ADDR_INGRESO, ingreso);
   EEPROM.get(ADDR_TURNO, turno);
   EEPROM.get(ADDR_PROBABILIDAD_INVASION, probabilidadInvasion);
 }
@@ -248,8 +249,8 @@ void guardarVariablesEnEEPROM() {
   EEPROM.put(ADDR_RECURSOS, recursos);
   EEPROM.put(ADDR_LIMITE_RECURSOS, limiteRecursos);
   EEPROM.put(ADDR_DINERO, dinero);
+  EEPROM.get(ADDR_INGRESO, ingreso);
   EEPROM.put(ADDR_TURNO, turno);
   EEPROM.put(ADDR_PROBABILIDAD_INVASION, probabilidadInvasion);
 }
-
 
